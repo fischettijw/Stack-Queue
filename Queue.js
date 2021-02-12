1412077fa05330eb5cae16b56ffa12e8f649e455
Joseph Fischetti <fischettijw@gmail.com>
2021-02-12 14:21:11 -0500

    starting QUEUE

--- Queue.js
+++ Queue.js
@@ -1,3 +1,19 @@
+// https://www.youtube.com/watch?v=OjdSaqQcsMo&list=PLeLwVYBodOL0npQy3bHOOAGH-6Uf-HY2d&index=22
+
 class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+    peek() {
+
+    }
+
+    enqueue(data) {
+
+    }
+
 
 }
